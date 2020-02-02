a80971380471e6d5c35886cd8e2cb2c625235676
Prabhakar Acharya <acharyaP03@gmail.com>
2020-02-02 18:59:46 +1100

    get request

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+// use Illuminate\Http\Resources\Json\Resource;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
+        //by default all the return json will be wrapped in data object, Resource::withoutWrapping(); will remove data
+        // Resource::withoutWrapping();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//List Articles
+Route::get('articles', 'ArticleController@index');
+
+//List Single Article
+Route::get('article/{id}', 'ArticleController@show');
+
+//Create Articles
+Route::post('article', 'ArticleController@store');
+
+//Update Articles
+Route::put('articles', 'ArticleController@store');
+
+//Delete Articles
+Route::delete('articles', 'ArticleController@destroy');
